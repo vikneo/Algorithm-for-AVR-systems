11314a6310414c16601ad3b1ed1ddf7fbec98e6c
Viktor <vm-viktor@mail.ru>
2024-01-19 09:27:55 +0700

    added urls.py

--- system_avr/system_avr/urls.py
+++ system_avr/system_avr/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-preview-panel/', admin.site.urls),
+    path('', include('firmwares.urls')),
+    path('account/', include('users.urls')),
 ]
