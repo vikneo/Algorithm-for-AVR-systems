2be7fcbbf00aacbde4b049b21d214be151ca29a9
Viktor <vm-viktor@mail.ru>
2024-01-22 14:15:48 +0700

    update templates for upload file

--- system_avr/firmwares/urls.py
+++ system_avr/firmwares/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import ProductListView
 
+app_name = 'product'
+
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product')
+    path('', ProductListView.as_view(), name='index')
 ]
