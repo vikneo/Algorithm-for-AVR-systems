a7a364e34e3af22e6c360948ad820d766b372055
Viktor <vm-viktor@mail.ru>
2024-04-23 12:58:52 +0700

    added func the get_count_for_id

--- system_avr/utils/config.py
+++ system_avr/utils/config.py
@@ -1,3 +1,20 @@
+from firmwares.models import Product
+
 
 # константа для среза фраз
 LENGTH = 25
+
+# константа для определения кол-ва символов в ID продукта
+COUNT_SYMBOL = 4
+
+
+def get_count_for_id(product_id) -> str:
+    """
+    Возращает строку с количеством символов для ID.
+    Максимальная длина хранится в константе COUNT_SYMBOL.
+
+    Например:
+        ID = 23 то значение вернется  0023
+        ID = 123 то значение вернется  0123
+    """
+    return '0' * (COUNT_SYMBOL - len(product_id)) + product_id if len(product_id) < COUNT_SYMBOL else product_id
