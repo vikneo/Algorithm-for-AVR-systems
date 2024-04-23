90aa65d1051196376245789470519734a7e6d1d5
Viktor <vm-viktor@mail.ru>
2024-04-23 15:19:59 +0700

    added url to auth

--- system_avr/users/urls.py
+++ system_avr/users/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from .views import RegisterUserView, LoginUserView
+
+
+app_name = 'profile'
+
 urlpatterns = [
-    
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('register/', RegisterUserView.as_view(), name='register'),
 ]
