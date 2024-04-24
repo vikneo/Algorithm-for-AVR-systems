c4fca7f4f13a0e44afac855105d184bc7b300262
Viktor <vm-viktor@mail.ru>
2024-04-24 12:49:32 +0700

    added url for log out of the system

--- system_avr/users/urls.py
+++ system_avr/users/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import RegisterUserView, LoginUserView
+from .views import (
+    RegisterUserView,
+    LoginUserView,
+    LogOutUserView,
+    )
 
 
 app_name = 'profile'
 
 urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
+    path('logout/', LogOutUserView.as_view(), name='logout'),
     path('register/', RegisterUserView.as_view(), name='register'),
 ]
