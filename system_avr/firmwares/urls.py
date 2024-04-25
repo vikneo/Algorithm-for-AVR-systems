e21b3bbaf581152889dd5284b4b9ee0a4d1a8f68
Viktor <vm-viktor@mail.ru>
2024-04-25 14:08:06 +0700

    added path for search

--- system_avr/firmwares/urls.py
+++ system_avr/firmwares/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 
-from .views import SubjectDetailView, SubjectListView, ProductView, ProductListView
+from .views import (
+    SubjectDetailView,
+    SubjectListView,
+    SearchView,
+    ProductView,
+    ProductListView,
+    )
 
 app_name = 'product'
 
@@ -8,5 +14,6 @@ urlpatterns = [
     path('', SubjectListView.as_view(), name='subject'),
     path('subject/<slug:slug>/', SubjectDetailView.as_view(), name='subject_detail'),
     path('product/<slug:slug>/', ProductView.as_view(), name='product_detail'),
-    path('products/', ProductListView.as_view(), name='product_list')
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('search/', SearchView.as_view(), name='search'),
 ] 
