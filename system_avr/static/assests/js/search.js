42a4a8f8264ed455573f16d6bd1102ec42fa225c
Viktor <vm-viktor@mail.ru>
2024-04-26 16:03:24 +0700

    added js file for field serach

---
+++ system_avr/static/assests/js/search.js
@@ -0,0 +1,20 @@
+function searchID() {
+    let search = document.getElementById('search')[0].value;
+
+    btn_search.onclick = function(event) {
+        if (event.which == 1) {
+            console.log(`${event.which}; ${search}`);
+            form.onsubmit = function () { return true;}
+        }
+    }
+
+    if (search == 0) {
+        console.log('В строке поиска должно быть число больше нуля!');
+        form.onsubmit = function () { return false;}
+        }
+    
+    if (!Number.isInteger(Number(search))) {
+        alert('В строке поиска должно быть только целое число!')
+        form.onsubmit = function () { return false;}
+    }
+}
