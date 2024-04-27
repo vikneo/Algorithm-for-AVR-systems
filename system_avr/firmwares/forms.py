19fa3e7f3885cbaf7e2a24bcdac7174d42088c39
Viktor <vm-viktor@mail.ru>
2024-04-27 14:18:00 +0700

    added form for create orders

--- system_avr/firmwares/forms.py
+++ system_avr/firmwares/forms.py
@@ -1,9 +1,83 @@
 from django import forms
-from .models import Client
+
+from .models import (
+    Product,
+)
 
 
 class CSVImportForm(forms.Form):
-    """
-    
-    """
+    """ """
+
     csv_file = forms.FileField()
+
+
+class CreatedOrderForm(forms.ModelForm):
+    """
+    Форма для создания заявок для систем АВР
+    """
+
+    id_product = forms.CharField(
+        label="ID проекта",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-input",
+                "placeholder": "ID проекта, например 7484",
+            },
+        ),
+    )
+    client = forms.CharField(
+        label='Клиент',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-input',
+                "placeholder": "Клиент",
+            },
+        ),
+    )
+    subject = forms.CharField(
+        label='Объект',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-input',
+                "placeholder": "Название объекта",
+            },
+        ),
+    )    
+    name = forms.CharField(
+        label='Название шкафа',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-input',
+                "placeholder": "Название шкафа",
+            },
+        ),
+    )
+    relay = forms.CharField(
+        label='Тип реле',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-input',
+                "placeholder": "например: ПР200",
+            },
+        ),
+    )
+    note = forms.CharField(
+        label='Тип реле',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-input',
+                "placeholder": "Примечание",
+            },
+        ),
+    )
+
+    class Meta:
+        model = Product
+        fields = [
+            'id_product',
+            'client',
+            'subject',
+            'name',
+            'relay',
+            'note'
+        ]
