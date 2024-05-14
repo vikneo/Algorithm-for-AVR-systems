4c8ff9d5614eaf16c32f25cc4315118661ca1b58
Viktor <vm-viktor@mail.ru>
2024-05-14 14:12:17 +0700

    added task - order_created

--- system_avr/firmwares/tasks.py
+++ system_avr/firmwares/tasks.py
@@ -1,14 +1,11 @@
+from system_avr.celery import app
 from django.core.mail import send_mail
 
-from models import Order
+from .models import Order
 from system_avr.settings import EMAIL_HOST_USER
 
-from celery import task
 
-
-
-
-@task
+@app.task
 def order_created(order_id):
     """
     Задача для отправки сообщения на почту при оформлении заявки
@@ -18,12 +15,13 @@ def order_created(order_id):
     message = f"Оформлена заявка с №-{order.id}.\n \
         Проект: ID-{order.id_product}.\n \
         Объект: {order.subject}.\n"
-    
+    print('Отправляем сообщение')
     mail_send = send_mail(
         subject=subject,
         message=message,
         from_email=EMAIL_HOST_USER,
         recipient_list=[EMAIL_HOST_USER, ]
     )
+    print('Сообщение отправлено')
 
     return mail_send
