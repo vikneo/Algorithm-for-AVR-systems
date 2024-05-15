5c521741f9b3807f7d00501ab166ab3cca243f14
Viktor <vm-viktor@mail.ru>
2024-05-15 10:37:59 +0700

    the parameter of the "user" field has been changed of the Order model

--- system_avr/firmwares/models.py
+++ system_avr/firmwares/models.py
@@ -221,7 +221,7 @@ class Order(models.Model):
     """
     Модель для заказов систем АВР
     """
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name='Автор заявки')
+    user = models.CharField(max_length=180, verbose_name='Автор заявки')
     id_product = models.IntegerField(verbose_name='ID Продукта', db_index=True)
     client = models.CharField(max_length=100, verbose_name='Клиент')
     subject = models.CharField(max_length=100, verbose_name='Объект')
