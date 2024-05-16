70358cdc41d4ae60eee47923f3de5cdb5a8d0050
Viktor <vm-viktor@mail.ru>
2024-05-16 16:15:53 +0700

    created sorting to left_sidebar

--- system_avr/static/assests/js/search.js
+++ system_avr/static/assests/js/search.js
@@ -28,3 +28,20 @@ function addToReestr () {
         return false;
     }
 }
+
+function Filter() {
+    document.getElementById("filterSet").classList.toggle(".show");
+  }
+  // Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName(".dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
