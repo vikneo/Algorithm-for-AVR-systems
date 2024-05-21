c92604afa7fe31f2a3bf7fce7ac4da0c4a6981b2
Viktor <vm-viktor@mail.ru>
2024-05-21 11:56:02 +0700

    save slug in the Product model (name+subject+id)

--- system_avr/firmwares/signals.py
+++ system_avr/firmwares/signals.py
@@ -14,7 +14,6 @@ from .models import (
 @receiver(pre_save, sender=Client)
 @receiver(pre_save, sender=Subjects)
 @receiver(pre_save, sender=SmartRelay)
-@receiver(pre_save, sender=Product)
 def get_slugify_save(sender, instance, **kwargs) -> None:
     """
     Перед сохранением созданной записи в БД проверяется поле "slug",
@@ -22,6 +21,17 @@ def get_slugify_save(sender, instance, **kwargs) -> None:
     """
     if not instance.slug:
         instance.slug = slugify(instance.name)
+
+
+@receiver(pre_save, sender=Product)
+def get_slugify_product_save(sender, instance, **kwargs) -> None:
+    """
+    Перед сохранением созданной записи в БД проверяется поле "slug",
+    если поле с пустым значчение, то метод "get_slug_save" заполняет поле
+    """
+    if not instance.slug:
+        name_slug = f'{instance.subject.name}_{instance.name}_{instance.id_product}'
+        instance.slug = slugify(name_slug)
 
 
 @receiver(pre_save, sender=Subjects)
