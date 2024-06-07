daf878e24f9eb236de8c466948100e75de3cfb0d
Viktor <vm-viktor@mail.ru>
2024-06-07 14:53:29 +0700

    added content for status_product

--- system_avr/static/assests/js/check_status.js
+++ system_avr/static/assests/js/check_status.js
@@ -1,3 +1,16 @@
-const span = document.querySelectorAll('.status_def');
+const spanStatus = document.querySelectorAll('.status_def');
 
-console.log(span)
+function handleClick(e) {
+    console.log(e.currentTarget)
+}
+
+spanStatus.forEach(stat => {
+    if (stat.innerText === "В работе") {
+        stat.classList.add("color-status-yellow");
+        
+    } else if (stat.innerText === 'Проверка') {
+        stat.classList.add("color-status-red");
+    } else {
+        stat.classList.add("color-status-green");
+    };
+});
