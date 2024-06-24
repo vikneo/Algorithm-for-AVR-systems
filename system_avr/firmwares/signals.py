3ba85f56e1de22c82eeb2b519d32828b5fcffbb9
Viktor <vm-viktor@mail.ru>
2024-06-24 16:00:15 +0700

    added logic for saving unique field "slug" in the module signals.py

--- system_avr/firmwares/signals.py
+++ system_avr/firmwares/signals.py
@@ -8,15 +8,17 @@ from .models import (
     Subjects,
     Product,
     SmartRelay,
-    ProductImage,
+    Order,
 )
 
 from datetime import datetime
+from random import randint
 
 
 @receiver(pre_save, sender=Client)
 @receiver(pre_save, sender=Subjects)
 @receiver(pre_save, sender=SmartRelay)
+@receiver(pre_save, sender=Order)
 def get_slugify_save(sender, instance, **kwargs) -> None:
     """
     Перед сохранением созданной записи в БД проверяется поле "slug",
@@ -24,18 +26,17 @@ def get_slugify_save(sender, instance, **kwargs) -> None:
     """
     if not instance.slug:
         instance.slug = slugify(instance.name)
+        while sender.objects.filter(slug= instance.slug):
+            instance.slug = ''
+            instance.slug = f"{slugify(instance.name)}-{''.join([chr(randint(97, 122)) for _ in range(5)])}"
 
 
 @receiver(pre_save, sender=Product)
-def get_slugify_product_save(sender, instance, **kwargs) -> None:
+def get_product_save(sender, instance, **kwargs) -> None:
     """
     Перед сохранением созданной записи в БД проверяется поле "slug",
     если поле с пустым значчение, то метод "get_slug_save" заполняет поле
     """
-    if not instance.slug:
-        name_slug = f"{instance.subject.name}_{instance.name}_{instance.id_product}"
-        instance.slug = slugify(name_slug)
-
     if instance.date_check:
         instance.status = 1
     elif instance.date_ready:
